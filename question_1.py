0a5bbe85a9d97274bc4af7cd1d10c50ea430fa0b
Johannes Fuest <johannes.fuest@tum.de>
2024-10-05 14:12:47 -0700

    hw1 q3 added

---
+++ question_1.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def poly_short(x: float) -> float:
+    return (x - 5)**8
+
+def ply_long(x: float) -> float:
+    return (x**8 - 40 * x**7 + 700*x**6 - 7000 * x**5 + 43750 * x**4 - 175000 * x**3 + 437500 * x**2 - 625000 * x + 390625)
+
+if __name__=="__main__":
+    x = np.linspace(4.0, 6.0, 201)
+    y_short = poly_short(x)
+    y_long = ply_long(x)
+    roundoff = np.abs(y_long - y_short) / np.abs(y_long)
+    
+    # Plot the roundoff error using a log scale
+    plt.plot(x, roundoff)
+    plt.yscale('log')
+    plt.xlabel('x')
+    plt.ylabel('Roundoff Error')
+    plt.title('Roundoff Error of Short and Long Polynomial')
+    plt.show()
+
+    
